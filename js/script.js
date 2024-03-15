655729d8c47fe5c4d6f6a836ac65cfd508300a24
hnnhcmmngs <hannahcummings94@gmail.com>
2024-03-15 13:10:15 -0400

    Create function to display books on page
    The books need to be displayed on the webpage as the user adds them
    to the library. The displayBooks function now does this. It loops
    over the books in the myLibrary array and takes the info from each
    book, adds it to a card div, and then appends that div to the library
    container element that was created in the index file. Some dummy books
    were created to demonstrate this functionality.

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+const library = document.querySelector("#library");
 const myLibrary = [];
 
 function Book(title, author, pages, read) {
@@ -13,3 +14,28 @@ function Book(title, author, pages, read) {
 function addBookToLibrary(book) {
     myLibrary.push(book);
 }
+
+function displayBooks() {
+    for (book of myLibrary) {
+        let b = document.createElement("div");
+        let title = document.createElement("div");
+        title.textContent = `Title: ${book.title}`;
+        let author = document.createElement("div");
+        author.textContent = `Author: ${book.author}`;
+        let pages = document.createElement("div");
+        pages.textContent = `${book.pages} pages`;
+        let read = document.createElement("div");
+        read.textContent = `${book.read ? "Read" : "Not Read"}`;
+        b.style.border = "3px solid blue";
+        b.appendChild(title);
+        b.appendChild(author);
+        b.appendChild(pages);
+        b.appendChild(read);
+        library.appendChild(b);
+    }
+}
+
+theHungerGames = new Book("The Hunger Games", "Suzanne Collins", 384, true);
+prideAndPrejudice = new Book("Pride and Prejudice", "Jane Austen", 259, false);
+addBookToLibrary(theHungerGames);
+addBookToLibrary(prideAndPrejudice);
