5b4e53073d7f62995e254170de9e907e8b635959
hnnhcmmngs <hannahcummings94@gmail.com>
2024-03-18 16:53:44 -0400

    Add New Book display functionality
    Before, the dialog box that allowed the user to input a new book's
    information popped up, but when the user submitted the form, the
    book was not added to the library display. Now, the new book appears
    on the display. There is form validation to prevent the user from
    inputting incomplete book entries. There is also a cancel button to
    cancel the action and add no new books. Whether a new book is added
    or the form is cancelled, the form is reset when it is closed.

--- js/script.js
+++ js/script.js
@@ -3,6 +3,8 @@ const library = document.querySelector("#library");
 const newBook = document.querySelector("#newbook");
 const dialog = document.querySelector("#bookinput");
 const addBook = document.querySelector("#addbook");
+const bookForm = document.querySelector("form");
+const cancel = document.querySelector("#cancel");
 
 function Book(title, author, pages, read) {
     this.title = title;
@@ -18,23 +20,27 @@ function addBookToLibrary(book) {
     myLibrary.push(book);
 }
 
+function displayBook(book) {
+    let b = document.createElement("div");
+    let title = document.createElement("div");
+    title.textContent = `Title: ${book.title}`;
+    let author = document.createElement("div");
+    author.textContent = `Author: ${book.author}`;
+    let pages = document.createElement("div");
+    pages.textContent = `${book.pages} ${book.pages == 1 ? "page" : "pages"}`;
+    let read = document.createElement("div");
+    read.textContent = `${book.read ? "Read" : "Not Read"}`;
+    b.style.border = "3px solid blue";
+    b.appendChild(title);
+    b.appendChild(author);
+    b.appendChild(pages);
+    b.appendChild(read);
+    library.appendChild(b);
+}
+
 function displayBooks() {
     for (book of myLibrary) {
-        let b = document.createElement("div");
-        let title = document.createElement("div");
-        title.textContent = `Title: ${book.title}`;
-        let author = document.createElement("div");
-        author.textContent = `Author: ${book.author}`;
-        let pages = document.createElement("div");
-        pages.textContent = `${book.pages} pages`;
-        let read = document.createElement("div");
-        read.textContent = `${book.read ? "Read" : "Not Read"}`;
-        b.style.border = "3px solid blue";
-        b.appendChild(title);
-        b.appendChild(author);
-        b.appendChild(pages);
-        b.appendChild(read);
-        library.appendChild(b);
+        displayBook(book);
     }
 }
 
@@ -47,4 +53,20 @@ displayBooks();
 
 newBook.addEventListener("click", () => {
     dialog.showModal();
+});
+
+cancel.addEventListener("click", (e) => {
+    e.preventDefault();
+    bookForm.reset();
+    dialog.close();
+})
+
+bookForm.addEventListener("submit", () => {
+    let inputBook = new Book(document.querySelector("#title").value,
+                            document.querySelector("#author").value,
+                            parseInt(document.querySelector("#pages").value),
+                            document.querySelector("#read").checked);
+    bookForm.reset();
+    myLibrary.push(inputBook);
+    displayBook(inputBook);
 });
